2d5e70cc8ed2787739c3e4da392cc64d6f0dab23
nart38 <sezer_1966_sezgin@hotmail.com>
2024-03-08 10:23:45 +0300

    imlement read file logic

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,29 @@
 const std = @import("std");
 const panic = std.debug.panic;
+const lexer = @import("lexer.zig");
+// const parser = @import("parser.zig");
 
 pub fn main() !void {
-    const argv = std.os.argv;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const argv = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, argv);
+
     if (argv.len != 2) {
-        panic("Usage: teeny myprogram.tt");
+        panic("\nERROR: Incorrect usage.\nUsage: teeny myprogram.tt", .{});
     }
+    const source_file_path: []const u8 = argv[1][0..];
+    var file = try std.fs.cwd().openFile(source_file_path, .{});
+    defer file.close();
+
+    const reader = file.reader();
+    var source = std.ArrayList(u8).init(allocator);
+    defer source.deinit();
+
+    reader.streamUntilDelimiter(source.writer(), 0, null) catch |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    };
 }
